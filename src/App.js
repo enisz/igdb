17f0f5c430f7b38b93a1252d70855bbfcae91584
Enisz Abdalla <enisz87@gmail.com>
2021-05-11 15:43:40 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import jQuery from 'jquery';
-import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DocumentationPage from './pages/DocumentationPage';
 import SearchPage from './pages/SearchPage';
@@ -8,13 +8,13 @@ window.jQuery = jQuery;
 
 export default function App() {
   return (
-    <HashRouter basename="/igdb">
+    <BrowserRouter basename="/igdb">
       <Switch>
-        <Redirect from="/" to="/home" exact />
-        <Route path="/home" component={HomePage} />
+        <Route path="/" component={HomePage} />
         <Route path="/documentation" component={DocumentationPage} exact />
         <Route path="/search" component={SearchPage} exact />
+        <Redirect to="/" />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
