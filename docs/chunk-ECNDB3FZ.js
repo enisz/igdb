27dddac870ae9d78eb8b696477860d8e6d35ecb0
enisz <33585488+enisz@users.noreply.github.com>
2024-03-04 22:16:20 +0000

    Deploying to documentation from @ enisz/igdb@710178ce9b17f92041d5235465e7c5c9ee102757 🚀

---
+++ docs/chunk-ECNDB3FZ.js
@@ -0,0 +1,1 @@
+import{Db as r,Kb as s,ka as o,sb as i,tb as a}from"./chunk-2CJAWYAV.js";var c=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-not-found"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,d){n&1&&(i(0,"p"),r(1,"not-found works!"),a())}});let t=e;return t})();export{c as NotFoundComponent};
